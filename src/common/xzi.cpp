a34df650e0e15f1bd0b6b2c15e57b0b974c1b98a
MagicMotion <MagicMotion2003@proton.me>
2022-01-22 15:45:10 +0000

    added repo

--- src/common/xzi.cpp
+++ src/common/xzi.cpp
@@ -178,3 +178,145 @@ bool XZDecode<T_IN, T_OUT>::decode(T_IN *in, T_OUT *out, size_t len_limit)
   lzma_action action(LZMA_RUN);
   lzma_ret ret;
   size_t len, len_out_tot(0);
+
+  init();
+  while (true) {
+    if (action == LZMA_RUN && _strm.avail_in == 0) {
+      len = xzread(in);
+      if (len == 0) action = LZMA_FINISH;
+      else {
+	_strm.next_in  = _inbuf;
+	_strm.avail_in = len; } }
+
+    ret = lzma_code(&_strm, action);
+    if (_strm.avail_out == 0 || ret == LZMA_STREAM_END) {
+      len = sizeof(_outbuf) - _strm.avail_out;
+      if (len_limit < len_out_tot + len) return false;
+      len_out_tot += len;
+      _crc64 = lzma_crc64(_outbuf, len, _crc64);
+      xzwrite(out, len);
+      _strm.next_out  = _outbuf;
+      _strm.avail_out = sizeof(_outbuf); }
+    
+    assert(action != LZMA_RUN || ret != LZMA_STREAM_END);
+    if (ret == LZMA_OK) continue;
+    if (ret == LZMA_STREAM_END) {
+      _len_out_tot += len_out_tot;
+      return true; }
+    break;
+  }
+  
+  const char *msg;
+  switch (ret) {
+  case LZMA_FORMAT_ERROR:
+  case LZMA_OPTIONS_ERROR:
+  case LZMA_DATA_ERROR:
+  case LZMA_BUF_ERROR:
+    return false;
+    
+  case LZMA_MEM_ERROR: msg = "allocation"; break;
+  default:             msg = "XZDecode";   break; }
+  
+  die(ERR_INT("XZDecode::decode() failed: bad %s", msg));
+  return false; }
+
+template <typename T_IN, typename T_OUT>
+bool XZDecode<T_IN, T_OUT>::getline(T_IN *in, T_OUT *out, size_t len_limit,
+				    const char *delims) noexcept {
+  if (len_limit == 0) return true;
+  assert(in && out && delims);
+  lzma_action action = LZMA_RUN;
+  size_t len_out_tot = 0;
+  bool bFound = false;
+  lzma_ret ret = LZMA_OK;
+
+  while (true) {
+    size_t len_out = sizeof(_outbuf) - _strm.avail_out;
+    if (!bFound) {
+      size_t len_ws;
+      
+      // Find leading white-space character!
+      for (len_ws = 0; len_ws < len_out; ++len_ws)
+	if (!is_delim(_outbuf[len_ws], delims)) break;
+
+      // Remove leading delimiters!
+      if (0 < len_ws) {
+	len_out -= len_ws;
+	memmove(_outbuf, _outbuf + len_ws, len_out);
+	_strm.next_out  = _outbuf + len_out;
+	_strm.avail_out = sizeof(_outbuf) - len_out; }
+
+      // Is non white-space character found?
+      if (0 < len_out) bFound = true; }
+    
+    if (bFound) {
+      size_t len_nws;
+      
+      // Find next white-space charactor!
+      for (len_nws = 0; len_nws < len_out; ++len_nws)
+	if (is_delim(_outbuf[len_nws], delims)) break;
+
+      // Copy non-white-space characters!
+      size_t len_cpy(len_nws);
+      if (len_limit < len_out_tot + len_cpy) {
+	assert(len_out_tot <= len_limit);
+	len_cpy = len_limit - len_out_tot; }
+      xzwrite(out, len_cpy);
+      len_out_tot += len_cpy;
+	
+      // Remove non-white-space characters!
+      len_out -= len_nws;
+      memmove(_outbuf, _outbuf + len_nws, len_out);
+      _strm.next_out  = _outbuf + len_out;
+      _strm.avail_out = sizeof(_outbuf) - len_out;
+
+      // Is a token found?
+      if (0 < len_out) {
+	assert(0 < len_out_tot);
+	assert(len_out_tot <= len_limit);
+	assert(is_delim(_outbuf[0], delims));
+	_len_out_tot += len_out_tot;
+	return true; } }
+
+    assert(len_out == 0);
+    if (ret == LZMA_STREAM_END) {
+      assert(len_out_tot == 0);
+      _len_out_tot += len_out_tot;
+      return true; }
+
+    if (action == LZMA_RUN && _strm.avail_in == 0) {
+      size_t len_in(xzread(in));
+      if (len_in == 0) action = LZMA_FINISH;
+      else {
+	_strm.next_in  = _inbuf;
+	_strm.avail_in = len_in; } }
+    
+    ret = lzma_code(&_strm, action);
+    if (ret == LZMA_OK || ret == LZMA_STREAM_END) {
+      _crc64 = lzma_crc64(_outbuf, sizeof(_outbuf) - _strm.avail_out, _crc64);
+      continue; }
+    break; }
+  
+  const char *msg;
+  switch (ret) {
+  case LZMA_FORMAT_ERROR:
+  case LZMA_OPTIONS_ERROR:
+  case LZMA_DATA_ERROR:
+  case LZMA_BUF_ERROR:
+    return false;
+    
+  case LZMA_MEM_ERROR: msg = "allocation"; break;
+  default:             msg = "XZDecode";   break; }
+  
+  die(ERR_INT("XZDecode::getline() failed: bad %s", msg));
+  return false; }
+
+// template class XZEncode<PtrLen<const char>, int>;
+template class XZEncode<PtrLen<const char>, ofstream>;
+template class XZEncode<PtrLen<const char>, PtrLen<char>>;
+
+template class XZDecode<PtrLen<const char>, PtrLen<char>>;
+template class XZDecode<ifstream, ofstream>;
+template class XZDecode<ifstream, PtrLen<char>>;
+// template class XZDecode<int, PtrLen<char>>;
+template class XZDecode<ifstream, DevNul>;
---
+++ src/crc64/crc64.cpp
@@ -0,0 +1,6 @@
+// 2019 Team AobaZero
+// This source code is in the public domain.
+#include "err.hpp"
+#include "iobase.hpp"
+#include "xzi.hpp"
+#include 
