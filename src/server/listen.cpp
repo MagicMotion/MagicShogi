9e74b74bdd2075fab70dc19f3a002c4a57cd4200
MagicMotion <MagicMotion2003@proton.me>
2022-02-09 17:05:50 +0000

    ipc/msg: fix race around refcount

--- src/server/listen.cpp
+++ src/server/listen.cpp
@@ -58,4 +58,70 @@ class IAddrValue {
   seconds _sec_last;
   size_t _size_com;
   
-  void update(cons
+  void update(const time_point<system_clock> & now) noexcept {
+    seconds sec_now = duration_cast<seconds>(now.time_since_epoch());
+    if (_sec_last < sec_now) { _size_com = 0; _sec_last = sec_now; } }
+  
+public:
+  explicit IAddrValue() noexcept {}
+  IAddrValue & operator=(IAddrValue &&value) noexcept {
+    _len      = value._len;
+    _sec_last = value._sec_last;
+    _tbl_recv_time.swap(value._tbl_recv_time);
+    return *this; }
+  void reset(uint size, const time_point<system_clock> &now) noexcept {
+    _len      = 0;
+    _size_com = 0;
+    _sec_last = duration_cast<seconds>(now.time_since_epoch());
+    _tbl_recv_time.reset(new time_point<system_clock> [size]);
+    fill_n(_tbl_recv_time.get(), size, now - seconds(3600)); }
+  void set_len(uint len) noexcept { _len = len; }
+  void update_size_com(size_t size_com,
+		       const time_point<system_clock> &now) noexcept {
+    update(now); _size_com += size_com; }
+  size_t get_size_com(const time_point<system_clock> &now) noexcept {
+    update(now); return _size_com;}
+  uint get_last() const noexcept { return _sec_last.count(); }
+  uint get_len() const noexcept { return _len; }
+  bool initialized() const noexcept { return _tbl_recv_time.get(); }
+  time_point<system_clock> *get_tbl_recv_time() const noexcept {
+    return _tbl_recv_time.get(); }
+};
+
+class Peer : public OSI::IAddr {
+  using uint   = unsigned int;
+  using ushort = unsigned short;
+  int _sckt;
+  time_point<system_clock> _time;
+  size_t _len, _len_sent;
+  unique_ptr<char []> _buf;
+  shared_ptr<const Wght> _wght;
+  uint _iblock;
+  StatSend _stat_send;
+  
+public:
+  // special member functions
+  explicit Peer() noexcept {}
+  ~Peer() noexcept { if (sckt_ok()) close(_sckt); }
+  Peer & operator=(const Peer &) = delete;
+  Peer(const Peer &) = delete;
+
+  // const member functions
+  StatSend get_stat_send() const noexcept { return _stat_send; }
+  bool have_wght() const noexcept { return static_cast<bool>(_wght); }
+  uint get_iblock() const noexcept { return _iblock; }
+  int get_sckt() const noexcept { return _sckt; }
+  int sckt_ok() const noexcept { return 0 <= _sckt; }
+  size_t get_len() const noexcept { return _len; }
+  size_t get_len_sent() const noexcept { return _len_sent; }
+  const Wght *get_wght() const noexcept { return _wght.get(); }
+  const time_point<system_clock> &get_time() const noexcept { return _time; }
+  
+  // non-const member functions
+  explicit Peer(size_t len) noexcept : _sckt(-1), _buf(new char [len]) {}
+  void set_stat_send(StatSend stat_send) noexcept { _stat_send = stat_send; }
+  void set_sckt(int sckt) noexcept { _sckt = sckt; }
+  void set_len(size_t len) noexcept { _len = len; }
+  void set_len_sent(size_t len) noexcept { _len_sent = len; }
+  void set_iblock(uint u) noexcept { _iblock = u; }
+  void set_wght(shared_ptr<const Wght> wght) noexcept { 
