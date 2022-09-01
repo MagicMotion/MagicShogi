a3bf5033d8339867e8f0d6523f016cfab151475a
MagicMotion <MagicMotion2003@proton.me>
2022-09-01 21:33:35 +0000

    metrics

--- src/usi-engine/bona/gencap.cpp
+++ src/usi-engine/bona/gencap.cpp
@@ -198,4 +198,117 @@ b_gen_captures( const tree_t * restrict ptree, unsigned int * restrict pmove )
 	    }
 	  else { *pmove++ = utemp; }
 	}
-   
+    }
+
+  bb_piece = BB_BKNIGHT;
+  while( BBTest( bb_piece ) )
+    {
+      ifrom = LastOne( bb_piece );
+      Xor( ifrom, bb_piece );
+
+      bb_desti = abb_b_knight_attacks[ifrom];
+      bb_desti.p[0] &= bb_movable.p[0];
+      bb_desti.p[1] &= bb_capture.p[1];
+      bb_desti.p[2] &= bb_capture.p[2];
+
+      while ( BBTest( bb_desti ) )
+	{
+	  ito = LastOne( bb_desti );
+	  Xor( ito, bb_desti );
+
+	  utemp = ( To2Move(ito) | From2Move(ifrom)
+		    | Cap2Move(-BOARD[ito]) | Piece2Move(knight) );
+	  if      ( ito < A7 ) { *pmove++ = utemp | FLAG_PROMO; }
+	  else if ( ito < A6 )
+	    {
+	      *pmove++ = utemp | FLAG_PROMO;
+	      if ( UToCap(utemp) ) { *pmove++ = utemp; }
+	    }
+	  else { *pmove++ = utemp; }
+	}
+    }
+
+  return pmove;
+}
+
+
+unsigned int * CONV
+w_gen_captures( const tree_t * restrict ptree, unsigned int * restrict pmove )
+{
+  bitboard_t bb_movable, bb_capture, bb_piece, bb_desti;
+  unsigned int utemp;
+  int ito, ifrom;
+
+  bb_capture = BB_BOCCUPY;
+  BBNot( bb_movable, BB_WOCCUPY );
+
+  bb_desti.p[2] = BB_WPAWN_ATK.p[2] & bb_movable.p[2];
+  bb_desti.p[1] = BB_WPAWN_ATK.p[1] & bb_capture.p[1];
+  bb_desti.p[0] = BB_WPAWN_ATK.p[0] & bb_capture.p[0];
+  while ( BBToU( bb_desti ) )
+    {
+      ito = FirstOne( bb_desti );
+      Xor( ito, bb_desti );
+
+      ifrom = ito - 9;
+      utemp = ( To2Move(ito) | From2Move(ifrom) | Cap2Move(BOARD[ito])
+		| Piece2Move(pawn) );
+      if ( ito > I4 ) { utemp |= FLAG_PROMO; }
+      *pmove++ = utemp;
+    }
+
+  bb_piece = BB_WSILVER;
+  while ( BBTest( bb_piece ) )
+    {
+      ifrom = FirstOne( bb_piece );
+      Xor( ifrom, bb_piece );
+
+      BBAnd( bb_desti, bb_capture, abb_w_silver_attacks[ifrom] );
+      while ( BBTest( bb_desti ) )
+	{
+	  ito = FirstOne( bb_desti );
+	  Xor( ito, bb_desti );
+
+	  utemp = ( To2Move(ito) | From2Move(ifrom) | Cap2Move(BOARD[ito])
+		    | Piece2Move(silver) );
+	  if ( ito > I4 || ifrom > I4 ) { *pmove++ = utemp | FLAG_PROMO; }
+	  *pmove++ = utemp;
+	}
+    }
+
+  bb_piece = BB_WTGOLD;
+  while( BBTest( bb_piece ) )
+    {
+      ifrom = FirstOne( bb_piece );
+      Xor( ifrom, bb_piece );
+
+      BBAnd( bb_desti, bb_capture, abb_w_gold_attacks[ifrom] );
+      while ( BBTest( bb_desti ) )
+	{
+	  ito = FirstOne( bb_desti );
+	  Xor( ito, bb_desti );
+
+	  *pmove++ = ( To2Move(ito) | From2Move(ifrom)
+		       | Cap2Move(BOARD[ito])
+		       | Piece2Move(-BOARD[ifrom]) );
+	}
+    }
+
+  ifrom = SQ_WKING;
+  BBAnd( bb_desti, bb_capture, abb_king_attacks[ifrom] );
+  while ( BBTest( bb_desti ) )
+    {
+      ito = FirstOne( bb_desti );
+      Xor( ito, bb_desti );
+
+      *pmove++ = ( To2Move(ito) | From2Move(ifrom)
+		   | Cap2Move(BOARD[ito]) | Piece2Move(king) );
+    }
+
+  bb_piece = BB_WBISHOP;
+  while ( BBTest( bb_piece ) )
+    {
+      ifrom = FirstOne( bb_piece );
+      Xor( ifrom, bb_piece );
+
+      AttackBishop( b
