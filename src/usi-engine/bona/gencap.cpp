df3b7578e4a955d6cd5531a3e499da4249bf9e6b
MagicMotion <MagicMotion2003@proton.me>
2022-09-02 20:02:28 +0000

    Update Readme.txt

--- src/usi-engine/bona/gencap.cpp
+++ src/usi-engine/bona/gencap.cpp
@@ -311,4 +311,117 @@ w_gen_captures( const tree_t * restrict ptree, unsigned int * restrict pmove )
       ifrom = FirstOne( bb_piece );
       Xor( ifrom, bb_piece );
 
-      AttackBishop( b
+      AttackBishop( bb_desti, ifrom );
+      bb_desti.p[2] &= bb_movable.p[2];
+      if ( ifrom > I4 )
+	{
+	  bb_desti.p[1] &= bb_movable.p[1];
+	  bb_desti.p[0] &= bb_movable.p[0];
+	}
+      else {
+	bb_desti.p[1] &= bb_capture.p[1];
+	bb_desti.p[0] &= bb_capture.p[0];
+      }
+
+      while ( BBTest( bb_desti ) )
+	{
+	  ito = FirstOne( bb_desti );
+	  Xor( ito, bb_desti );
+
+	  utemp = ( To2Move(ito) | From2Move(ifrom)
+		    | Cap2Move(BOARD[ito]) | Piece2Move(bishop) );
+	  if ( ito > I4 || ifrom > I4 ) { utemp |= FLAG_PROMO; }
+	  *pmove++ = utemp;
+	}
+    }
+
+  bb_piece = BB_WROOK;
+  while ( BBTest( bb_piece ) )
+    {
+      ifrom = FirstOne( bb_piece );
+      Xor( ifrom, bb_piece );
+
+      AttackRook( bb_desti, ifrom );
+      bb_desti.p[2] &= bb_movable.p[2];
+      if ( ifrom > I4 )
+	{
+	  bb_desti.p[1] &= bb_movable.p[1];
+	  bb_desti.p[0] &= bb_movable.p[0];
+	}
+      else {
+	bb_desti.p[1] &= bb_capture.p[1];
+	bb_desti.p[0] &= bb_capture.p[0];
+      }
+
+      while ( BBTest( bb_desti ) )
+	{
+	  ito = FirstOne( bb_desti );
+	  Xor( ito, bb_desti );
+
+	  utemp = ( To2Move(ito) | From2Move(ifrom)
+		    | Cap2Move(BOARD[ito]) | Piece2Move(rook) );
+	  if ( ito > I4 || ifrom > I4 ) { utemp |= FLAG_PROMO; }
+	  *pmove++ = utemp;
+	}
+    }
+
+  bb_piece = BB_WHORSE;
+  while ( BBTest( bb_piece ) )
+    {
+      ifrom = FirstOne( bb_piece );
+      Xor( ifrom, bb_piece );
+
+      AttackHorse( bb_desti, ifrom );
+      BBAnd( bb_desti, bb_desti, bb_capture );
+      while ( BBTest( bb_desti ) )
+	{
+	  ito = FirstOne( bb_desti );
+	  Xor( ito, bb_desti );
+
+	  *pmove++ = ( To2Move(ito) | From2Move(ifrom)
+		       | Cap2Move(BOARD[ito]) | Piece2Move(horse) );
+	}
+    }
+
+  bb_piece = BB_WDRAGON;
+  while ( BBTest( bb_piece ) )
+    {
+      ifrom = FirstOne( bb_piece );
+      Xor( ifrom, bb_piece );
+
+      AttackDragon( bb_desti, ifrom );
+      BBAnd( bb_desti, bb_desti, bb_capture );
+      while ( BBTest( bb_desti ) )
+	{
+	  ito = FirstOne( bb_desti );
+	  Xor( ito, bb_desti );
+
+	  *pmove++ = ( To2Move(ito) | From2Move(ifrom)
+		       | Cap2Move(BOARD[ito]) | Piece2Move(dragon) );
+	}
+    }
+
+  bb_piece = BB_WLANCE;
+  while( BBTest( bb_piece ) )
+    {
+      ifrom = FirstOne( bb_piece );
+      Xor( ifrom, bb_piece );
+
+      bb_desti = AttackFile( ifrom );
+      BBAnd( bb_desti, bb_desti, abb_plus_rays[ifrom] );
+      bb_desti.p[2] &= bb_movable.p[2];
+      bb_desti.p[1] &= bb_capture.p[1];
+      bb_desti.p[0] &= bb_capture.p[0];
+
+      while ( BBTest( bb_desti ) )
+	{
+	  ito = FirstOne( bb_desti );
+	  Xor( ito, bb_desti );
+
+	  utemp = ( To2Move(ito) | From2Move(ifrom)
+		    | Cap2Move(BOARD[ito]) | Piece2Move(lance) );
+	  if      ( ito > I3 ) { *pmove++ = utemp | FLAG_PROMO; }
+	  else if ( ito > I4 )
+	    {
+	      *pmove++ = utemp | FLAG_PROMO;
+	      if ( UToCap(utemp) ) { *pmove++ =
