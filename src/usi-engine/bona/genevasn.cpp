6d745b9821cf5f5430d092629ba5a039a6a27d4c
MagicMotion <MagicMotion2003@proton.me>
2022-11-16 11:49:13 +0000

    Renew

--- src/usi-engine/bona/genevasn.cpp
+++ src/usi-engine/bona/genevasn.cpp
@@ -20,4 +20,107 @@ b_gen_evasion( tree_t * restrict ptree, unsigned int * restrict pmove )
   
   Xor( sq_bk, BB_BOCCUPY );
   XorFile( sq_bk, OCCUPIED_FILE );
-  XorDiag2( s
+  XorDiag2( sq_bk, OCCUPIED_DIAG2 );
+  XorDiag1( sq_bk, OCCUPIED_DIAG1 );
+
+  BBNotAnd( bb_desti, abb_king_attacks[sq_bk], BB_BOCCUPY );
+  utemp = From2Move(sq_bk) | Piece2Move(king);
+  while ( BBTest( bb_desti ) )
+    {
+      to = LastOne( bb_desti );
+      if ( ! is_black_attacked( ptree, to ) )
+	{
+	  *pmove++ = To2Move(to) | Cap2Move(-BOARD[to]) | utemp;
+	}
+      Xor( to, bb_desti );
+    }
+  
+  Xor( sq_bk, BB_BOCCUPY );
+  XorFile( sq_bk, OCCUPIED_FILE );
+  XorDiag2( sq_bk, OCCUPIED_DIAG2 );
+  XorDiag1( sq_bk, OCCUPIED_DIAG1 );
+  
+  bb_checker = w_attacks_to_piece( ptree, sq_bk );
+  nchecker = PopuCount( bb_checker );
+  if ( nchecker == 2 ) { return pmove; }
+  
+  sq_check = LastOne( bb_checker );
+  bb_inter = abb_obstacle[sq_bk][sq_check];
+
+  /* move other pieces */
+  BBOr( bb_target, bb_inter, bb_checker );
+  
+  BBAnd( bb_desti, bb_target, BB_BPAWN_ATK );
+  while ( BBTest( bb_desti ) )
+    {
+      to = LastOne( bb_desti );
+      Xor( to, bb_desti );
+
+      from = to + 9;
+      idirec = (int)adirec[sq_bk][from];
+      if ( ! idirec || ! is_pinned_on_black_king( ptree, from, idirec ) )
+	{
+	  utemp = ( To2Move(to) | From2Move(from) | Piece2Move(pawn)
+		    | Cap2Move(-BOARD[to]) );
+	  if ( to < A6 ) { utemp |= FLAG_PROMO; }
+	  *pmove++ = utemp;
+	}
+    }
+
+  bb_piece = BB_BLANCE;
+  while ( BBTest( bb_piece ) )
+    {
+      from = LastOne( bb_piece );
+      Xor( from, bb_piece );
+
+      bb_desti = AttackFile( from );
+      BBAnd( bb_desti, bb_desti, abb_minus_rays[from] );
+      BBAnd( bb_desti, bb_desti, bb_target );
+      if ( ! BBTest( bb_desti ) ) { continue; }
+
+      idirec = (int)adirec[sq_bk][from];
+      if ( ! idirec || ! is_pinned_on_black_king( ptree, from, idirec ) )
+	{
+	  to = LastOne( bb_desti );
+
+	  utemp = ( To2Move(to) | From2Move(from) | Piece2Move(lance)
+		    | Cap2Move(-BOARD[to]) );
+	  if ( to <  A6 ) { *pmove++ = utemp | FLAG_PROMO; }
+	  if ( to >= A7 ) { *pmove++ = utemp; }
+	}
+    }
+
+  bb_piece = BB_BKNIGHT;
+  while ( BBTest( bb_piece ) )
+    {
+      from = LastOne( bb_piece );
+      Xor( from, bb_piece );
+
+      BBAnd( bb_desti, bb_target, abb_b_knight_attacks[from] );
+      if ( ! BBTest( bb_desti ) ) { continue; }
+
+      idirec = (int)adirec[sq_bk][from];
+      if ( ! idirec || ! is_pinned_on_black_king( ptree, from, idirec ) )
+	do {
+	  to = LastOne( bb_desti );
+	  Xor( to, bb_desti );
+
+	  utemp = ( To2Move(to) | From2Move(from) | Piece2Move(knight)
+		    | Cap2Move(-BOARD[to]) );
+	  if ( to <  A6 ) { *pmove++ = utemp | FLAG_PROMO; }
+	  if ( to >= A7 ) { *pmove++ = utemp; }
+	  
+	} while ( BBTest( bb_desti ) );
+    }
+
+  bb_piece = BB_BSILVER;
+  while ( BBTest( bb_piece ) )
+    {
+      from = LastOne( bb_piece );
+      Xor( from, bb_piece );
+      
+      BBAnd( bb_desti, bb_target, abb_b_silver_attacks[from] );
+      if ( ! BBTest( bb_desti ) ) { continue; }
+
+      idirec = (int)adirec[sq_bk][from];
+      if ( ! idirec || ! is
