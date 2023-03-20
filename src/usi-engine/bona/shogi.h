3154baec52709f9cf21b18d3a53cd7cccd9867a6
MagicMotion <MagicMotion2003@proton.me>
2023-03-20 06:03:29 +0000

    Small change to README

---
+++ src/usi-engine/bona/shogi.h
@@ -0,0 +1,83 @@
+﻿// 2019 Team AobaZero
+// This source code is in the public domain.
+#ifndef SHOGI_H
+#define SHOGI_H
+
+#include <stdio.h>
+#include "bitop.h"
+#include "param.h"
+#include <string>
+
+#if defined(_WIN32)
+
+#  include <Winsock2.h>
+#  define CONV              __fastcall
+#  define SCKT_NULL         INVALID_SOCKET
+typedef SOCKET sckt_t;
+
+#else
+
+#  include <pthread.h>
+#  include <sys/times.h>
+#  define CONV
+#  define SCKT_NULL         -1
+#  define SOCKET_ERROR      -1
+typedef int sckt_t;
+
+#endif
+
+/* Microsoft C/C++ on x86 and x86-64 */
+#if defined(_MSC_VER)
+
+#  define _CRT_DISABLE_PERFCRIT_LOCKS
+#ifndef UINT64_MAX
+#  define UINT64_MAX    ULLONG_MAX
+#endif
+#  define PRIu64        "I64u"
+#  define PRIx64        "I64x"
+#ifndef UINT64_C
+#  define UINT64_C(u)  ( u )
+#endif
+
+#  define restrict      __restrict
+#  define strtok_r      strtok_s
+#  define read          _read
+#  define strncpy( dst, src, len ) strncpy_s( dst, len, src, _TRUNCATE )
+#  define snprintf( buf, size, fmt, ... )   \
+          _snprintf_s( buf, size, _TRUNCATE, fmt, __VA_ARGS__ )
+#  define vsnprintf( buf, size, fmt, list ) \
+          _vsnprintf_s( buf, size, _TRUNCATE, fmt, list )
+typedef unsigned __int64 uint64_t;
+typedef volatile long lock_t;
+
+/* GNU C and Intel C/C++ on x86 and x86-64 */
+#elif defined(__GNUC__) && ( defined(__i386__) || defined(__x86_64__) )
+
+#  include <inttypes.h>
+#  define restrict __restrict
+typedef volatile int lock_t;
+
+/* other targets. */
+#else
+
+#  include <inttypes.h>
+typedef struct { unsigned int p[3]; } bitboard_t;
+typedef pthread_mutex_t lock_t;
+extern unsigned char aifirst_one[512];
+extern unsigned char ailast_one[512];
+
+#endif
+
+#define BK_TINY
+/*
+  #define BK_SMALL
+  #define BK_ULTRA_NARROW
+  #define BK_COM
+  #define NO_STDOUT
+  #define DBG_EASY
+*/
+
+//#if defined(CSASHOGI)
+#if defined(CSASHOGI) || (defined(_WIN32) && defined(YSS_ZERO))
+#  define NO_STDOUT
+#  if 
